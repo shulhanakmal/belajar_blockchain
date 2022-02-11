79148efddd9502cb9831bd190bae0423e2e496bb
Shulhan Akmal <shulhanakmal@gmail.com>
2022-02-11 10:35:01 +0700

    pembuatan navigasi halaman

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+const HalamanSatu = () => {
+    return <h1>Hello World</h1>;
+}
+
+const HalamanDua = () => {
+    return <h1>Halaman Dua</h1>;
+}
 
 const App = () => {
-    return <h1>Hello World</h1>;
+    return(
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" exact element={<HalamanSatu />} />
+                    <Route path="/HalamanDua" exact element={<HalamanDua />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
